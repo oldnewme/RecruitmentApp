1bc9b6fcf127c1e225ba77d519e4ec9fa1b46c6d
Christian Antonio <cantonio@Christians-MacBook-Pro.local>
2021-02-23 16:59:16 +0100

    update config vars

--- src/integration/ApplicationDAO.js
+++ src/integration/ApplicationDAO.js
@@ -10,7 +10,7 @@ class ApplicationDAO {
             process.env.DB_NAME,
             process.env.DB_USER,
             process.env.DB_PASS,
-            {host: /*process.env.DB_HOST*/process.env.DATABASE_URL, dialect: process.env.DB_DIALECT}
+            {host: process.env.DB_HOST, dialect: process.env.DB_DIALECT}
         );
         Applicant.createModel(this.database);
         Recruiter.createModel(this.database);
