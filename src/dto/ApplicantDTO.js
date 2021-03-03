9ff383395cab205c198ab0b7e8b2fcdc68ac132e
Christian Antonio <cantonio@Christians-MacBook-Pro.local>
2021-03-03 14:53:54 +0100

    fix conflict in applicantDTO

--- src/dto/ApplicantDTO.js
+++ src/dto/ApplicantDTO.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * An Applicant that will send in job application(s)
  */
@@ -20,26 +19,3 @@ class ApplicantDTO{
 }
 
 module.exports = ApplicantDTO;
-=======
-/**
- * An Applicant that will send in job application(s)
- */
-
-class ApplicantDTO{
-    /**
-     *
-     * @param {ApplicantDTO} applicantDTO a data transfer object containing info needed
-     * to register an applicant
-     */
-    constructor({firstName, lastName, email, dob, username, password}){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.dob = dob;
-        this.username = username;
-        this.password = password;
-    }
-}
-
-module.exports = ApplicantDTO;
->>>>>>> 647ded02b0c1adcd74473a2222a987743ee64fed
