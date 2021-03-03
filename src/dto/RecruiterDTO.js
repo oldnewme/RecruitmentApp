36a22b544231601a977fd919839a4b11f6d9a551
Christian Antonio <cantonio@Christians-MacBook-Pro.local>
2021-03-03 14:52:08 +0100

    fix conflict in recruiterDTO

--- src/dto/RecruiterDTO.js
+++ src/dto/RecruiterDTO.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * An Recruiter that will send in job application(s)
  */
@@ -21,29 +20,3 @@ class RecruiterDTO{
 }
 
 module.exports = RecruiterDTO;
-=======
-/**
- * An Recruiter that will send in job application(s)
- */
-
-class RecruiterDTO{
-    /**
-     *
-     * @param {RecruiterDTO} recruiterDTO a data transfer object containing info needed
-     * to register an Recruiter
-     */
-
-class RecruiterDTO{
-    constructor(firstName, lastName, email, dob, username, password){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.dob = dob;
-        this.username = username;
-        this.password = password;
-    }
-
-}
-
-module.exports = RecruiterDTO;
->>>>>>> 647ded02b0c1adcd74473a2222a987743ee64fed
