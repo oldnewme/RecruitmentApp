a5973254100a2423c08ddcff514026bc4575fab0
albroms <albroms@kth.se>
2021-03-10 22:10:41 +0100

    ApplicationDAO test now relatively extensive for persons.

--- tests/integrationTests/ApplicationDAO.test.js
+++ tests/integrationTests/ApplicationDAO.test.js
@@ -7,20 +7,49 @@ const Person = require('../../src/model/Person');
 let connection = null;
 let applicationDAO = null;
 
-const newPerson = new PersonDTO(
-  {
-    "name": "amoasdfng",
-    "surame": "asdf",
-    "email": "qqqqqq@meme.com",
-    "ssn": "1970-01-01",
-    "roleId": "2",
-    "username": "asdfgsdd",
-    "password": "password"
-  }
-);
+const newPerson = new PersonDTO({
+  "name": "Among",
+  "surame": "Us",
+  "email": "sus@meme.com",
+  "ssn": "1970-01-01",
+  "roleId": 2,
+  "username": "amongus",
+  "password": "password"
+});
+const badEmail = new PersonDTO({
+  "name": "A",
+  "surname": "B",
+  "email": "invalid",
+  "ssn": "1970-01-01",
+  "roleId": 2,
+  "username": "badMailPerson",
+  "password": "password"
+});
+const sameUser = new PersonDTO({
 
-const applicant = '2';
-const recruiter = '1';
+});
+const sameEmail = new PersonDTO({
+  "name": "C",
+  "surname": "D",
+  "email": "sus@meme.com",
+  "ssn": "1970-01-01",
+  "roleId": 2,
+  "username": "sameEmailPerson",
+  "password": "password"
+
+});
+const validPerson = new PersonDTO({
+  "name": "Peter",
+  "surname": "Griffin",
+  "email": "peter@perhaps.com",
+  "ssn": "1965-08-23",
+  "roleId": 2,
+  "username": "peter",
+  "password": "password"
+});
+
+const applicant = 2;
+const recruiter = 1;
 
 beforeAll(async () => {
   connection = await connectToDB();
@@ -52,12 +81,60 @@ describe('tests for getPerson', () => {
     expect(foundUser.email).toBe(newPerson.email);
   });
   test('non-existing person', async() => {
-    let personNotFoundErrorThrown = false;
+    let personNotFound = false;
     try{const foundUser = await applicationDAO.getPerson("NotImportant");}
     catch(error){
-      personNotFoundErrorThrown = true;
+      personNotFound = true;
     }
-    expect(personNotFoundErrorThrown).toBe(true);
+    expect(personNotFound).toBe(true);
+  });
+});
+
+describe('tests for createPerson', () => {
+  test('invalid email format', async () => {
+    let caughtError = false;
+    try{
+      caughtError = await applicationDAO.createPerson(badEmail, badEmail.roleId);
+      await applicationDAO.destroyPerson(badEmail);
+    }
+    catch(error){
+      caughtError = true;
+    }
+    expect(caughtError).toBe(true);
+  });
+  test('duplicate email', async () => {
+    let duplicateEmail = false;
+    try{
+      duplicateEmail = await applicationDAO.createPerson(sameEmail, sameEmail.roleId);
+      await applicationDAO.destroyPerson(sameEmail);
+    }
+    catch(error){
+      duplicateEmail = true;
+    }
+    expect(duplicateEmail).toBe(true);
+  });
+  test('duplicate username', async () => {
+    let duplicateUsername = false;
+    try{
+      duplicateUsername = await applicationDAO.createPerson(sameUser, sameUser.roleId);
+      await applicationDAO.destroyPerson(sameUser);
+    }
+    catch(error){
+      duplicateUsername = true;
+    }
+    expect(duplicateUsername).toBe(true);
+  });
+  test('add another person', async () => {
+    let successfullyAdded = false;
+    try{
+      await applicationDAO.createPerson(validPerson, validPerson.roleId);
+      successfullyAdded = true;
+      await applicationDAO.destroyPerson(validPerson);
+    }
+    catch(error){
+      successfullyAdded = false;
+    }
+    expect(successfullyAdded).toBe(true);
   });
 });
 
@@ -84,8 +161,3 @@ const connectToDB = async () => {
   );
   return connection;
 };
-
-
-/*
-const clearDB = async () => {};
-*/
